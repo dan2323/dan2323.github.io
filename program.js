f8022d6852c428eb2597bd9ff4021fc37ce9f7f4
dan2323 <44407208+dan2323@users.noreply.github.com>
2018-10-30 18:33:19 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,5 +1,16 @@
+var pisica = false;
+
 function modifica()
 {
   document.getElementById("id_salut").innerHTML = "Hello";
+  
+  if(pisica == false){
+     document.getElementById("id_img").src = "cat.png";
+     pisica = true;
+}
+else{
   document.getElementById("id_img").src = "cat.png";
+  pisica = true;
+}
+  
 }
