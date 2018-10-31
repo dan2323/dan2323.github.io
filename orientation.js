19d280098e7f4c7ad39feddb8f9f1a9ab5ecebeb
dan2323 <44407208+dan2323@users.noreply.github.com>
2018-10-31 08:15:08 +0200

    Update orientation.js

--- orientation.js
+++ orientation.js
@@ -2,7 +2,7 @@ document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.
 
 window.addEventListener("deviceorientation", la_schimbare_gyro);
 
-function la_schimbare_gyro(e);
+function la_schimbare_gyro(e)
 {
 document.getElementById("id_x").innerHTML =e.beta;
 document.getElementById("id_y").innerHTML =e.gamma;
