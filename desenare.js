9cb8147f7b97b138f96572866dbfe8bfe2e4163a
dan2323 <44407208+dan2323@users.noreply.github.com>
2018-11-20 18:46:27 +0200

    Update desenare.js

--- desenare.js
+++ desenare.js
@@ -1,6 +1,8 @@
-document.getElementById("id_logic_version").inneriHTML = "Logic version = 2018.11.20.3";
+document.getElementById("id_logic_version").inneriHTML = "Logic version = 2018.11.20.4";
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstar", on_touch);
+canvas.addEventListener("touchmove", on_touch_move);
+
 var rect = canvas.getBoundingClientRect();
 
 
@@ -24,4 +26,25 @@ function on_touch(e)
 	 
 	 
 	 }
+
+//--------------------------------
+function  on_touch_move(e);
+
+{
+
+    for(var i = 0; i < e.changeTouches.lenght; i++);
+
+    {
+	
+	 var context = canvas.getContext("2d");
+	 context.beginPath();
+	 context.arc(e.changedTouches.item(i).pageX - rect.left, e.changedTouches.item(i).pagesY - rect.right, 
+	 20,
+	 0, 2 * Math.PI
+	 );
+	 context.stroke();
 	 
+	 }
+	 
+	 
+	 }	 
