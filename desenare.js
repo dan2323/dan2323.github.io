28900e9fdac2c72692813d4e3c46382cb8b9de0f
dan2323 <44407208+dan2323@users.noreply.github.com>
2018-11-20 19:15:43 +0200

    Update desenare.js

--- desenare.js
+++ desenare.js
@@ -4,7 +4,9 @@ canvas.addEventListener("touchstart", on_touch);
 canvas.addEventListener("touchmove", on_touch_move);
 
 var rect = canvas.getBoundingClientRect();
-
+//------------------------------------
+var lastX = 0;
+var lastY = 0;
 
 function on_touch(e)
 
@@ -35,15 +37,24 @@ e.preventDefault
     for(var i = 0; i < e.changeTouches.lenght; i++);
 
     {
-	
+	 context.lineWith = 40;
+
 	 var context = canvas.getContext("2d");
 	 context.beginPath();
 	 context.arc(e.changedTouches.item(i).pageX - rect.left, e.changedTouches.item(i).pagesY - rect.right, 
 	 20,
 	 0, 2 * Math.PI
 	 );
+	    context.lineWith = 40;
+	    context.moveTo(lastX-rect.left, lastY-rect.top);
+	    context.lineTo(e.changedTouches.item(i).pageY - rect.top);
+ lastX = e.changeTouches.item(i)pageX;
+ lastY = e.changeTouches.item(i)pageY;    
+	    
 	 context.stroke();
-	 
+	   
+lastX = e.changeTouches.item(i)pageX;
+lastY = e.changeTouches.item(i)pageY;	 
 	 }
 	 
 	 
