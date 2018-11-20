542eb28eee5cc3a9b3f3860819965b949c5b65be
dan2323 <44407208+dan2323@users.noreply.github.com>
2018-11-20 18:25:15 +0200

    desenare

---
+++ desenare.js
@@ -0,0 +1,26 @@
+document.getElementyById("id_logic_version").inneriHTML = "Logic version = 2018.11.20.0";
+var canvas = document.getElementyById("id_canvas");
+canvas.addEventListener("touchstar", on_touch);
+
+
+function on_touch(e)
+
+{
+
+    for(var i = 0; i < e.changeTouches.lenght; i++);
+
+    {
+	
+	 var context = canvas.getContext("2d");
+	 context.beginPath();
+	 context.arc(e.changedTouches.item(i).pageX, e.changedTouches.item(i).pagesY, 
+	 20,
+	 0, 2 * Math.PI
+	 );
+	 context.stroke();
+	 
+	 }
+	 
+	 
+	 }
+	 
